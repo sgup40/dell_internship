8e4062d229e8b9533bd3e97dffbd51b6b9445e4e
Sumit Gupta IV <sumgupta3@publicisgroupe.net>
2018-11-11 16:22:39 +0530

    udated the data structure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into ATTR (ATTRID,IDENTIFIER,ATTRTYPEID,FIELD1,FIELD2,CREATED_DATE,UPDATED_DATE ) values (1,'Tyre',1,null,null,sysdate,sysdate);
+insert into ATTR (ATTRID,IDENTIFIER,ATTRTYPEID,FIELD1,FIELD2,CREATED_DATE,UPDATED_DATE ) values (2,'steering',1,null,null,sysdate,sysdate);
+insert into ATTR (ATTRID,IDENTIFIER,ATTRTYPEID,FIELD1,FIELD2,CREATED_DATE,UPDATED_DATE ) values (3,'gear',1,null,null,sysdate,sysdate);
+insert into ATTR (ATTRID,IDENTIFIER,ATTRTYPEID,FIELD1,FIELD2,CREATED_DATE,UPDATED_DATE ) values (4,'battery',1,null,null,sysdate,sysdate);
+insert into ATTR (ATTRID,IDENTIFIER,ATTRTYPEID,FIELD1,FIELD2,CREATED_DATE,UPDATED_DATE ) values (5,'window',1,null,null,sysdate,sysdate);
+
+
